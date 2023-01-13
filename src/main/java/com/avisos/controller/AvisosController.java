e1f1f7ca17f875b9d37ad58fb7fee489dc2745c2
BrunoRomeropmsv <brunoromeropmsv@gmail.com>
2023-01-13 17:04:27 -0300

    Controller update

--- src/main/java/com/avisos/controller/AvisosController.java
+++ src/main/java/com/avisos/controller/AvisosController.java
@@ -1,24 +1,50 @@
 package com.avisos.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.avisos.model.Avisos;
-import com.avisos.service.AvisosServcie;
+import com.avisos.repository.IAvisosRepository;
 
 @RestController
-@RequestMapping("/api/avisos")
+
 public class AvisosController {
 
-	@Autowired
-	private AvisosServcie service;
+
 	
-	@GetMapping
-	public List<Avisos> buscarTodos() {
-		return service.findAll();
+	@Autowired
+	private IAvisosRepository Iavisorepository;
+	
+	@GetMapping("/api/avisos/{id}")
+	public ResponseEntity<Avisos> buscaId(@PathVariable Long id) {
+		return Iavisorepository.findById(id).map((a) ->ResponseEntity.ok(a)).orElse(ResponseEntity.notFound().build());
+		
 	}
+	
+	
+	@GetMapping("/api/avisos")
+	public Stream<Object> buscaTodoscontem(){
+		return  Iavisorepository.findAll().stream().filter((a) -> a.getTitulo().contains("segundo")).map((a) ->ResponseEntity.ok(a)) ;
+	}
+	
+	
+	@GetMapping("/api/avisos/filtro")
+	public List<Avisos> buscaTodosFiltro(){
+		return Iavisorepository.findAll().stream().filter((a) -> a.getTitulo().charAt(0) == 'p').collect(Collectors.toList());
+	}
+	
+	@GetMapping("/api/avisos/full")
+	public List<Avisos> buscaTodos(){
+		return  Iavisorepository.findAll() ;
+	}
+	
+	
+	
 }
